84bf8fba8fc1240abafceb6d26ca269e6f089122
mohamed baraka <barakamohamed946@gmail.com>
2026-08-24 22:33:10 +0300

    add logout logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,4 +18,9 @@ export class AuthController {
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
+
+  @Post('logout')
+  logout() {
+    return this.authService.logout();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -108,4 +108,8 @@ export class AuthService {
       },
     };
   }
+
+  async logout() {
+    return;
+  }
 }
